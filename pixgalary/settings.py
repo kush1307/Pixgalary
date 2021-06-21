f5271acb484e3a0e7564a4f01f74a06958229162
kush1307 <kushkushalpatel@gmail.com>
2021-06-21 11:18:14 +0530

    feat: Added facebook auth and reset password functionality in pixgalary app.

--- pixgalary/settings.py
+++ pixgalary/settings.py
@@ -177,3 +177,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'pin-home'
 LOGIN_URL = 'login'
+
+# Email Config
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
