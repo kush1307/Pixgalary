dfe8e5a2909c5436e14d65f3dfdf17c0b85346d0
kush1307 <kushkushalpatel@gmail.com>
2021-07-06 16:13:06 +0530

    fix: Fixed facebook and google auth.

--- pins/models.py
+++ pins/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -19,6 +20,9 @@ class Pins(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('pin-detail', kwargs={'pk': self.pk})
 
 
 class SavePin(models.Model):
@@ -41,5 +45,8 @@ class Board(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('board-detail', kwargs={'pk': self.pk})
 
 
